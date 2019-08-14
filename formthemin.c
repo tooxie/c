b58095222ff8936520816d8a208aee0a17ada4de
Alvaro Mourino <alvaro@mourino.net>
2019-08-14 14:35:40 -0500

    Add top-voted solution for formthemin.c

--- formthemin.c
+++ formthemin.c
@@ -70,3 +70,24 @@ int main (int argc, char* argv[]) {
 
     printf("%llu\n", minValue(values, argc-1));
 }
+
+/*
+ * Top-voted solution:
+ *
+ * #include <stdlib.h>
+ *
+ * unsigned long long minValue(const int* values, const size_t len)
+ * {
+ *   int min = 0;
+ *   int digits[10] = {0};
+ *   for (int i=0;i<len;++i)
+ *   {
+ *     digits[values[i]]++;
+ *   }
+ *   for (int i=1;i<=9;++i)
+ *   {
+ *     if (digits[i]) {min *= 10; min += i;}
+ *   }
+ *   return min;
+ * }
+ */
